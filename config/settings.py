7767f73d80fc471826e877fdc513d3c9edef1812
abdelrahman-ibrahem <abdo.ibrahem1122@gmail.com>
2023-10-31 16:39:06 +0200

    update allowed hosts in settings

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$$0su1v^ecpg7-83h4t#u60drd$-nl^!%t@&$69hq-borrg^%8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
